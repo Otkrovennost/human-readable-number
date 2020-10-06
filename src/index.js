2fb477b19687afb84be3214974a771ea8af636b4
Otkrovennost <Otkrovennost@mail.ru>
2020-10-06 20:55:55 +0300

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const numbersTens = [0, 1, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
 module.exports = function toReadable (number) {
-  
+    let result = '';
+
+    if (number < 20) {
+        result = numbers[number];
+    } else if (number < 100) {
+        if (number % 10 === 0) {
+            result = numbersTens[(number - number % 10) / 10];
+        } else {
+            result = numbersTens[(number - number % 10) / 10] + ' ' + numbers[number % 10];
+        }  
+    } else {
+        result = numbers[(number - number % 100) / 100] + ' hundred';
+        if (number % 100 === 0) {
+            return result;
+        } 
+        number = number % 100;
+        if (number < 20) {
+            result = result + ' ' + numbers[number];
+        } else if (number < 100) {
+            if (number % 10 === 0) {
+                result = result + ' ' + numbersTens[(number - number % 10) / 10];
+            } else {
+                result = result + ' ' + numbersTens[(number - number % 10) / 10] + ' ' + numbers[number % 10];
+            }
+        }
+    }
+
+    return result;
 }
